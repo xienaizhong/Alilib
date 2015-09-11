3bc2a38b87245b861cc7a0e67e5dc786537bffd8
a <a@192.168.1.148>
2015-09-11 13:30:36 +0800

    alilib v1

--- src/alilibs/AliFile.java
+++ src/alilibs/AliFile.java
@@ -32,7 +32,7 @@ public class AliFile {
     }
     
     
-    
+    //
     public static void append(String filename,List<String>list ) throws IOException
     {
         File f = new File(filename);
